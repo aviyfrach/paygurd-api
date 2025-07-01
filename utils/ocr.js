fd01f07584cf55e0bb5b23d1a7b950affc07f69d
aviyfrach <aviyfrach@gmail.com>
2025-07-01 14:15:56 +0300

    Update ocr.js

--- utils/ocr.js
+++ utils/ocr.js
@@ -1,1 +1,9 @@
+import Tesseract from "tesseract.js";
 
+export async function extractTextFromImage(imageUrl) {
+  const result = await Tesseract.recognize(imageUrl, "eng", {
+    logger: (m) => console.log(m),
+  });
+
+  return result.data.text;
+}
